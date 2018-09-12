0dbd385be22664a3c7e7e0c699f1bfa2c0b2b33e
adamsapple <hardrock.is.my.life@gmail.com>
2018-09-13 01:01:43 +0900

    プロジェクト ファイルを追加します。

---
+++ UiSample/UiSample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UiSample.Models;
+
+namespace UiSample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ UiSample/UiSample/Views/TabbedPage1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UiSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCurrentPageChanged(object sender, EventArgs e)
+        {
+            //Title = CurrentPage.Title;
+        }
+
+        /// <summary>
+        /// (Android)Backボタンを押された時の処理
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            var nav = (Parent as NavigationPage)?.Navigation;
+
+            if (nav == null)
+            {
+                return true;
+            }
+
+            /// Navのスタックが詰まれていたら戻る
+            if (nav.NavigationStack.Count > 1)
+            {
+                /// 戻る
+                nav.PopAsync();
+            }
+            else
+            {
+                /// MasterPageの表示/非表示切替え
+                //this.IsPresented = !this.IsPresented;
+            }
+
+            return true;
+        }
+    }
+}
