de7cd29ccd34b7857ed531c15aff9a337f784e17
adamsapple <hardrock.is.my.life@gmail.com>
2018-09-14 11:36:13 +0900

    バインディング例追加

---
+++ UiSample/UiSample/ViewModels/Page1ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace UiSample.ViewModels
+{
+    public class Page1ViewModel : BaseViewModel
+    {
+        int andgle = 50;
+        public int Angle
+        {
+            get => andgle;
+            set => SetProperty(ref andgle, value);
+        }
+
+        public ICommand PutAngleCommand { get; }
+
+        public Page1ViewModel()
+        {
+            PutAngleCommand = new Command(() => System.Diagnostics.Debug.WriteLine(message: $"Execute:{Angle}"));
+        }
+    }
+}
